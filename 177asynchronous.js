f6d3b5a0c400bebc7864b90e79f24dbea71ee7b1
MoonKabir <moonkabir4@gmail.com>
2020-12-05 12:39:13 +0600

    Asynchronous file updated

---
+++ 177asynchronous.js
@@ -0,0 +1,25 @@
+//-----178 asynchronous  
+
+// console.log("I am Line One")
+// setTimeout(()=>{
+//     console.log("I am Line two")
+// },3000)
+// setTimeout(()=>{
+//     console.log("I am Line three")
+// },1000)
+// console.log("I am Line four")
+
+//-----179 store data
+
+// function sayMyName(name){
+//     let result 
+//     setTimeout(() => {
+//         result = name
+//         console.log('I have done...')
+//     }, 3000)
+//     return result
+// }
+// let output = sayMyName('Moon Kabir')
+// console.log(output)
+
+// 180
