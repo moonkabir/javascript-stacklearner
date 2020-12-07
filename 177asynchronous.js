176a7c493c8b9680e065f3b22705fe223b09bfa2
MoonKabir <moonkabir4@gmail.com>
2020-12-07 22:56:27 +0600

    ajax data test code

--- 177asynchronous.js
+++ 177asynchronous.js
@@ -22,4 +22,59 @@
 // let output = sayMyName('Moon Kabir')
 // console.log(output)
 
-// 180
+//------ 184 ajax defination
+
+//-------185 ajax callback
+
+// const xhr = new XMLHttpRequest()
+// xhr.open('get', 'http://jsonplaceholder.typicode.com/users')
+
+// xhr.onreadystatechange = function(e){
+//     if(xhr.readyState === 4){
+//         if(xhr.status === 200){
+//             let users = JSON.parse(xhr.response)
+//             console.log(users)
+//         }else{
+//             console.log(shr.status)
+//         }
+//     }
+// }
+
+// xhr.send()
+
+
+// function getRequest(url,callback) {
+//     const xhr = new XMLHttpRequest()
+//     xhr.open('get', url)
+
+//     xhr.onreadystatechange = function(e){
+//         if(xhr.readyState === 4){
+//             if(xhr.status === 200){
+//                 let response = JSON.parse(xhr.response)
+//                 callback(null, response)
+//             }else{
+//                 callback(xhr.status,null)
+//             }
+//         }
+//     }
+
+//     xhr.send()
+// }
+
+// getRequest('http://jsonplaceholder.typicode.com/users', (err, res)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log(res)
+//         res.forEach(r=> alert(r.name))
+//     }
+// })
+
+// getRequest('http://jsonplaceholder.typicode.com/posts', (err, res)=>{
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log(res)
+//         res.forEach(r=>console.log(r.title))
+//     }
+// })
