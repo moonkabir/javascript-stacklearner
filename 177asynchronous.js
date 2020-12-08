ce1b608993489cda1bf55126fc4c451aa5bc07e1
MoonKabir <moonkabir4@gmail.com>
2020-12-08 11:14:43 +0600

    here end of asynchronous chapter

--- 177asynchronous.js
+++ 177asynchronous.js
@@ -78,3 +78,71 @@
 //         res.forEach(r=>console.log(r.title))
 //     }
 // })
+
+//------186 callback not always asynchronous
+
+// let arr = [1, 2, 3, 4]
+
+// let sqrArr = arr.map(v => v * v) 
+// console.log(sqrArr)
+
+// function asyncMap(arr, cb){
+//     return arr.map(v =>{
+//         setTimeout(cb.bind(null, v), 0)
+//         setTimeout(() => (v), 0)
+//     })
+// }
+
+// let qbArr = asyncMap(arr, v =>{
+//     console.log(v)
+// })
+// console.log(qbArr)
+
+// ------187not callback
+
+// callback system isSecureContext
+
+//----- 188 promise
+// age resolved asto ekon 'fulfilled' ase
+
+// let p1 = new Promise((resolve, reject) =>{
+//     setTimeout(resolve, 5000, 'one')
+// })
+
+// let p2 = new Promise((resolve, reject) =>{
+//     setTimeout(resolve, 3000, 'two')
+// })
+
+// p1.then((v)=>{
+//     console.log(p1)
+// }).catch((e)=>{
+//     console.log(e)
+// })
+
+// p2.then((v)=>{
+//     console.log(p2)
+// }).catch((e)=>{
+//     console.log(e)
+// })
+
+function getIphone(isPassed){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if(isPassed){
+                resolve('I Have Got an Iphone')
+            }else{
+                reject(new Error('You Have Failed'))
+            }
+        }, 2000)
+    })
+}
+
+// true or false
+
+getIphone(true)
+    .then((res) => {
+        console.log(res)
+    })
+    .catch((e) => {
+        console.log(e)
+    })
