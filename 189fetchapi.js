6b846dc9e810efdb0871adacd4eb0e51964dbfb6
MoonKabir <moonkabir4@gmail.com>
2020-12-10 10:26:38 +0600

    async generator and end of this chapter

--- 189fetchapi.js
+++ 189fetchapi.js
@@ -120,42 +120,57 @@
 
 // --------192 async iterator
 
-let asyncIterable ={
-    [Symbol.asyncIterator](){
-        let i = 0;
-        return{
-            next(){
-                if(i<5){
-                    return Promise.resolve({
-                        value: i++,
-                        done: false
-                    })
-                }else{
-                    return Promise.resolve({
-                        done: true
-                    })
-                }
-            }
-        }
+// let asyncIterable ={
+//     [Symbol.asyncIterator](){
+//         let i = 0;
+//         return{
+//             next(){
+//                 if(i<5){
+//                     return Promise.resolve({
+//                         value: i++,
+//                         done: false
+//                     })
+//                 }else{
+//                     return Promise.resolve({
+//                         done: true
+//                     })
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// let iterate = asyncIterable[Symbol.asyncIterator]()
+// // use of iffi
+// // ;(async function (){
+// //     // let v = await iterate.next()
+// //     // console.log(v)
+// //     console.log(await iterate.next())
+// //     console.log(await iterate.next())
+// //     console.log(await iterate.next())
+// //     console.log(await iterate.next())
+// //     console.log(await iterate.next())
+// //     console.log(await iterate.next())
+// // })()
+// // --------193 for await loop
+
+// // ;(async function (){
+// //     for await (let v of asyncIterable){
+// //         console.log(v)
+// //     }
+// // })()
+
+// ----194 async generator
+
+async function* myAsyncGenerator() {
+    let i = 0;
+    while (true){
+        if(i>8)return
+        yield await Promise.resolve(i++)
     }
 }
-
-let iterate = asyncIterable[Symbol.asyncIterator]()
-// use of iffi
-// ;(async function (){
-//     // let v = await iterate.next()
-//     // console.log(v)
-//     console.log(await iterate.next())
-//     console.log(await iterate.next())
-//     console.log(await iterate.next())
-//     console.log(await iterate.next())
-//     console.log(await iterate.next())
-//     console.log(await iterate.next())
-// })()
-// --------193 for await loop
-
-;(async function (){
-    for await (let v of asyncIterable){
+;(async function(){
+    for await (let v of myAsyncGenerator()){
         console.log(v)
     }
 })()
