22d1e971a3915c2a589614c54c90a20cec228215
MoonKabir <moonkabir4@gmail.com>
2020-12-10 11:51:16 +0600

    dom theory basis topic

---
+++ 195dom.js
@@ -0,0 +1,14 @@
+// ---195 introduction DOM(Data Object Model)
+// ----196 WIndow Object 
+// -----198 & 199 selector
+
+// document.getElementById();
+// document.getElementsByClassName();
+// document.getElementsByTagName();
+// document.getElementsByName();
+// document.querySelector();
+// document.querySelectorAll();
+
+// -----200 getElement vs Query selectron
+// getElementBy use korle html collection pawa jay
+// querySelector use korle node list pawa jay
