2946e9ddd95344c87d1e809564a62322992a1aa4
MoonKabir <moonkabir4@gmail.com>
2020-12-15 09:38:40 +0600

    213 list and input event handlers

--- 195dom.js
+++ 195dom.js
@@ -217,3 +217,29 @@ function outputSkills(parent, skills){
     })
     parent.innerHTML = result
 }
+
+// -------213 list and input event handlers
+
+let list = document.getElementById('list')
+list.addEventListener('dblclick', function(event){
+    if(this.contains(event.target)){
+        let innerText = event.target.innerText
+        event.target.innerHTML = ""
+        let inputBox = createInputBox(innerText)
+        event.target.appendChild(inputBox)
+
+        inputBox.addEventListener('keypress', function(e){
+            if("Enter" == e.key){
+                event.target.innerHTML = e.target.value
+            }
+        })
+    }
+})
+
+function createInputBox(value){
+    let inp = document.createElement('input')
+    inp.type = 'text'
+    inp.className = 'form-control'
+    inp.value = value
+    return inp
+}
